6b258651851fa8a76e0584138be915ae18aca3bd
cmonoto <312204457@qq.com>
2017-04-22 14:04:11 -0400

    remove the sql text

--- pConfirmLogin.sql
+++
@@ -1,6 +0,0 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `pConfirmLogin`(uname varchar(128),pwd varchar(128))
-BEGIN
-SELECT e.eid,e.supervisor,e.firstname
-FROM employee as e
-WHERE e.username = uname AND e.password = pwd ;
-END
